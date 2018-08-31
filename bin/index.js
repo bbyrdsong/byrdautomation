f01b215d428a6d961274c01073f0efd291a4dba5
Brian Byrdsong <Brian.Byrdsong@ey.com>
2018-08-31 15:59:02 -0400

    adding git functionality

--- bin/index.js
+++ bin/index.js
@@ -2,9 +2,18 @@
 
 const program = require('commander');
 const configMover = require('../lib/config-mover');
+const gitCloner = require('../lib/git');
 const { version } = require('../package.json');
 
 program
-    .version(version)
+    .version(version);
+
+// move-config
+program
     .command('move-config', 'Moves proxy config files to and from desktop')
     .action(configMover());
+
+// git [project-name] from my github repo
+program
+    .command('git', 'Performs git clone against the bbyrdsong github repository')
+    .action(gitCloner());
